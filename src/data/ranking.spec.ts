ff99d0bad07f8454c879ecce93f863b7e482d938
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-02-16 23:46:32 +0700

    feat: ranking (#16)
    * feat: ranking draft part 1
    
    * style: fix prettier
    
    * feat(ranking): basic table
    
    * feat(ranking): load from csv
    
    * exp: 404 page

---
+++ src/data/ranking.spec.ts
@@ -0,0 +1,49 @@
+import { describe, expect, it } from "vitest";
+
+import { createRankingProps } from "./ranking";
+
+describe("Ranking Utilities", () => {
+  it("should sort scores", () => {
+    expect(
+      createRankingProps({
+        round: "Contest",
+        tasks: ["A", "B", "C"],
+        scores: [
+          { name: "Alice", scores: [100, 100, 100] },
+          { name: "Bob", scores: [100, 100, 90] },
+          { name: "Charlie", scores: [100, 100, 80] },
+        ],
+      })
+    ).toStrictEqual({
+      round: "Contest",
+      headers: ["Rank", "Name", "A", "B", "C", "Total"],
+      content: [
+        [1, "Alice", 100, 100, 100, 300],
+        [2, "Bob", 100, 100, 90, 290],
+        [3, "Charlie", 100, 100, 80, 280],
+      ],
+    });
+  });
+
+  it("tie case", () => {
+    expect(
+      createRankingProps({
+        round: "Contest",
+        tasks: ["A", "B", "C"],
+        scores: [
+          { name: "Alice", scores: [100, 100, 100] },
+          { name: "Bob", scores: [100, 100, 100] },
+          { name: "Charlie", scores: [100, 100, 100] },
+        ],
+      })
+    ).toStrictEqual({
+      round: "Contest",
+      headers: ["Rank", "Name", "A", "B", "C", "Total"],
+      content: [
+        [1, "Alice", 100, 100, 100, 300],
+        [1, "Bob", 100, 100, 100, 300],
+        [1, "Charlie", 100, 100, 100, 300],
+      ],
+    });
+  });
+});
---
+++ src/data/ranking.ts
@@ -0,0 +1,45 @@
+export type Ranking = {
+  round: string;
+  tasks: string[];
+  scores: Array<{ name: string; scores: number[] }>;
+};
+
+export function createRankingProps(ranking: Ranking) {
+  const { round, tasks, scores } = ranking;
+
+  const headers = ["Rank", "Name", ...tasks, "Total"];
+
+  const computed = scores.map((score) => [
+    0,
+    score.name,
+    ...score.scores,
+    score.scores.reduce((a, b) => a + b, 0),
+  ]);
+
+  computed.sort((a, b) => {
+    const aScore = +a.at(-1);
+    const bScore = +b.at(-1);
+
+    if (aScore > bScore) {
+      return -1;
+    } else if (aScore < bScore) {
+      return 1;
+    } else {
+      return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0;
+    }
+  });
+
+  for (let i = 0; i < computed.length; i++) {
+    computed[i][0] = i + 1;
+
+    if (i > 0 && computed[i].at(-1) === computed[i - 1].at(-1)) {
+      computed[i][0] = computed[i - 1][0];
+    }
+  }
+
+  return {
+    round,
+    headers,
+    content: computed,
+  };
+}
