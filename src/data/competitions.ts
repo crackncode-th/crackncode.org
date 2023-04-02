69179b51342e042d6f848afae2211501fd6803ae
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-04-03 00:27:08 +0700

    chore: Pre POSN2 2023:tada:

--- src/data/competitions.ts
+++ src/data/competitions.ts
@@ -143,14 +143,12 @@ export const competitions: Record<string, ICompetition[]> = {
       duration: "25 March 2023 19:00 - 22:00",
       link: "/special/23/pre-posn-2",
     },
-  ],
-  "Upcoming (2023)": [
     {
       round: "April",
       special: "Pre POSN 2",
       duration: "1 April 2023 19:00 - 22:00",
       link: "/special/23/pre-posn-2",
     },
-    ...generateMonths(5, 12),
   ],
+  "Upcoming (2023)": [...generateMonths(5, 12)],
 };
